ac0aade25751dc7c59d72194d0bbb341196aef90
salehibneomar <salehibneomar@gmail.com>
2023-11-18 14:56:47 +0600

    changed spacing

--- resources/views/template/includes/sidebar.blade.php
+++ resources/views/template/includes/sidebar.blade.php
@@ -4,21 +4,21 @@
         <ul class="metismenu" id="menu">
             <li>
                 <a href="{{ route('dashboard') }}">
-                    <span class="fa-solid fa-house pr-3"></span>
+                    <span class="fa-solid fa-house pr-2"></span>
                     <span class="nav-text">Home</span>
                 </a>
             </li>
             @if (Auth::user()->isAdmin() || Auth::user()->isSuperAdmin())
                 <li>
                     <a href="{{ route('deposit.requests') }}">
-                        <span class="fa-solid fa-filter-circle-dollar pr-3"></span>
+                        <span class="fa-solid fa-filter-circle-dollar pr-2"></span>
                         <span class="nav-text">Deposit Requests</span>
                     </a>
                 </li>
 
                 <li>
                     <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                        <span class="fa-solid fa-sliders pr-3"></span>
+                        <span class="fa-solid fa-sliders pr-2"></span>
                         <span class="nav-text">Settings</span>
                     </a>
                     <ul aria-expanded="false">
