8f2dfe38e39d6e707dd550dc7a1de05956896d2b
salehibneomar <salehibneomar@gmail.com>
2023-11-18 17:51:01 +0600

    style fixes

--- resources/views/pages/deposit/index.blade.php
+++ resources/views/pages/deposit/index.blade.php
@@ -1,70 +1,72 @@
 @extends('template.layouts.common-layout')
 @section('page_title')
-Deposit Requests
+    Deposit Requests
 @endsection
 
 
 @section('main_content')
-<div class="row px-1 mb-3">
-    <div class="col-12 p-md-0">
-        <div class="welcome-text px-md-4 px-1">
-            <h6>Manage Deposit Requests</h6>
-            <span style="font-size: 8pt;">Accept or Reject Deposit Requests</span>
-        </div>
-    </div>
-</div>
-<div class="row px-1">
-    <div class="col-12">
-        <div class="card">
-
-            <div class="card-body">
-                @if (count($allRequests) < 1) <p class="text-info"><small>No Requests!</small></p>
-                    @else
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>User Name</th>
-                                <th>Amount</th>
-                                <th>TransactionID</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($allRequests as $request)
-                            <tr>
-                                <td>{{ $request->user->username }}</td>
-                                <td>{{ $request->amount }}</td>
-                                <td>{{ $request->transaction_id }}</td>
-                                <td>
-                                    @if($request->deposit_status == 2)
-                                    <span class="badge bg-warning text-dark">Pending</span>
-                                    @elseif ($request->deposit_status == 1)
-                                    <span class="badge bg-success">Accepted</span>
-                                    @else
-                                    <span class="badge bg-danger">Rejected</span>
-                                    @endif
-                                </td>
-                                <td>
-                                    <a href="{{ route('deposit.details', ['id' => $request->id]) }}" class="btn btn-primary shadow btn-xs sharp">
-                                        <i class="fa fa-eye"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{ $allRequests->links() }}
-                    @endif
+    <div class="row px-1 mb-3">
+        <div class="col-12 p-md-0">
+            <div class="welcome-text px-md-4 px-1">
+                <h6>Manage Deposit Requests</h6>
+                <span style="font-size: 8pt;">Accept or Reject Deposit Requests</span>
             </div>
         </div>
     </div>
-</div>
+    <div class="row px-1">
+        <div class="col-12">
+            <div class="card">
+
+                <div class="card-body">
+                    @if (count($allRequests) < 1)
+                        <p class="text-info"><small>No Requests!</small></p>
+                    @else
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>User Name</th>
+                                    <th>Amount</th>
+                                    <th>TransactionID</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($allRequests as $request)
+                                    <tr>
+                                        <td>{{ $request->user->username }}</td>
+                                        <td>{{ $request->amount }}</td>
+                                        <td>{{ $request->transaction_id }}</td>
+                                        <td>
+                                            @if ($request->deposit_status == 2)
+                                                <span class="badge bg-warning text-white">Pending</span>
+                                            @elseif ($request->deposit_status == 1)
+                                                <span class="badge bg-success text-white">Accepted</span>
+                                            @else
+                                                <span class="badge bg-danger text-white">Rejected</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('deposit.details', ['id' => $request->id]) }}"
+                                                class="btn btn-info shadow btn-xs sharp">
+                                                <i class="fa fa-eye"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $allRequests->links() }}
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('extra_script')
-<script>
-    $('.delete').on('click', (e) => {
+    <script>
+        $('.delete').on('click', (e) => {
             e.preventDefault()
             const targetItem = e.currentTarget
             console.log(targetItem)
@@ -81,5 +83,5 @@ Deposit Requests
                 }
             });
         })
-</script>
+    </script>
 @endsection
